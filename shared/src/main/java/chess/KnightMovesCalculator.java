506ae9ad11ca24edf6f0d9e27a302e78ae9d8b4c
Bree Towne <bltowne@outlook.com>
2026-06-24 21:05:09 -0700

    imp(p0) basic knight structure

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -7,7 +8,102 @@ public class KnightMovesCalculator implements PieceMovesCalculator {
 
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1, 8), null));
+        ChessPiece piece = board.getPiece(myPosition);
+        ChessGame.TeamColor color = piece.getTeamColor();
+        Collection<ChessMove> moves = new ArrayList<>();
+        knightForward(board, myPosition, color, moves);
+        knightBackward(board, myPosition, color, moves);
+        knightLeft(board, myPosition, color, moves);
+        knightRight(board, myPosition, color, moves);
+        return moves;
+    }
+
+    private void knightForward(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color, Collection<ChessMove> moves) {
+        int x = myPosition.getRow();
+        int y = myPosition.getColumn() + 2;
+
+        if (y <= 8) {
+            ChessPosition leftEndPosition = new ChessPosition(x - 1, y);
+            if (x >= 1) {
+                ChessPiece leftEnemy = board.getPiece(leftEndPosition);
+                if (leftEnemy == null || leftEnemy.getTeamColor() != color) {
+                    moves.add(new ChessMove(myPosition, leftEndPosition, null));
+                }
+            }
+            ChessPosition rightEndPosition = new ChessPosition(x + 1, y);
+            if (x <= 8) {
+                ChessPiece rightEnemy = board.getPiece(leftEndPosition);
+                if (rightEnemy == null || rightEnemy.getTeamColor() != color) {
+                    moves.add(new ChessMove(myPosition, rightEndPosition, null));
+                }
+            }
+        }
+    }
+
+    private void knightBackward(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color, Collection<ChessMove> moves) {
+        int x = myPosition.getRow();
+        int y = myPosition.getColumn() - 2;
+
+        if (y >= 1) {
+            ChessPosition leftEndPosition = new ChessPosition(x - 1, y);
+            if (x >= 1) {
+                ChessPiece leftEnemy = board.getPiece(leftEndPosition);
+                if (leftEnemy == null || leftEnemy.getTeamColor() != color) {
+                    moves.add(new ChessMove(myPosition, leftEndPosition, null));
+                }
+            }
+            ChessPosition rightEndPosition = new ChessPosition(x + 1, y);
+            if (x <= 8) {
+                ChessPiece rightEnemy = board.getPiece(leftEndPosition);
+                if (rightEnemy == null || rightEnemy.getTeamColor() != color) {
+                    moves.add(new ChessMove(myPosition, rightEndPosition, null));
+                }
+            }
+        }
+    }
+
+    private void knightLeft(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color, Collection<ChessMove> moves) {
+        int x = myPosition.getRow() + 2;
+        int y = myPosition.getColumn();
+
+        if (x <= 8) {
+            ChessPosition leftEndPosition = new ChessPosition(x, y - 1);
+            if (y >= 1) {
+                ChessPiece leftEnemy = board.getPiece(leftEndPosition);
+                if (leftEnemy == null || leftEnemy.getTeamColor() != color) {
+                    moves.add(new ChessMove(myPosition, leftEndPosition, null));
+                }
+            }
+            ChessPosition rightEndPosition = new ChessPosition(x, y + 1);
+            if (y <= 8) {
+                ChessPiece rightEnemy = board.getPiece(leftEndPosition);
+                if (rightEnemy == null || rightEnemy.getTeamColor() != color) {
+                    moves.add(new ChessMove(myPosition, rightEndPosition, null));
+                }
+            }
+        }
+    }
+
+    private void knightRight(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color, Collection<ChessMove> moves) {
+        int x = myPosition.getRow() - 2;
+        int y = myPosition.getColumn();
+
+        if (x >= 1) {
+            ChessPosition leftEndPosition = new ChessPosition(x, y - 1);
+            if (y >= 1) {
+                ChessPiece leftEnemy = board.getPiece(leftEndPosition);
+                if (leftEnemy == null || leftEnemy.getTeamColor() != color) {
+                    moves.add(new ChessMove(myPosition, leftEndPosition, null));
+                }
+            }
+            ChessPosition rightEndPosition = new ChessPosition(x, y + 1);
+            if (y <= 8) {
+                ChessPiece rightEnemy = board.getPiece(leftEndPosition);
+                if (rightEnemy == null || rightEnemy.getTeamColor() != color) {
+                    moves.add(new ChessMove(myPosition, rightEndPosition, null));
+                }
+            }
+        }
     }
 
 }
