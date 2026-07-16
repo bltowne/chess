697e39f49ad4e2146906d7d56868efb79ebc7440
Bree Towne <bltowne@outlook.com>
2026-07-15 20:17:08 -0700

    imp(p3) fix code quality - knight moves duplication pt 2

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -22,7 +22,7 @@ public class KnightMovesCalculator implements PieceMovesCalculator {
         int y = myPosition.getColumn() + 2;
 
         if (y <= 8) {
-            checkX(board, myPosition, color, moves, x, y);
+            checkSides(board, myPosition, color, moves, x, y, "y");
         }
     }
 
@@ -31,35 +31,18 @@ public class KnightMovesCalculator implements PieceMovesCalculator {
         int y = myPosition.getColumn() - 2;
 
         if (y >= 1) {
-            checkX(board, myPosition, color, moves, x, y);
+            checkSides(board, myPosition, color, moves, x, y, "y");
         }
     }
 
-    private static void checkX(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color, Collection<ChessMove> moves, int x, int y) {
-        int lowX = x - 1;
-        if (lowX >= 1) {
-            ChessPosition leftEndPosition = new ChessPosition(lowX, y);
-            ChessPiece leftEnemy = board.getPiece(leftEndPosition);
-            if (leftEnemy == null || leftEnemy.getTeamColor() != color) {
-                moves.add(new ChessMove(myPosition, leftEndPosition, null));
-            }
-        }
-        int highX = x + 1;
-        if (highX <= 8) {
-            ChessPosition rightEndPosition = new ChessPosition(highX, y);
-            ChessPiece rightEnemy = board.getPiece(rightEndPosition);
-            if (rightEnemy == null || rightEnemy.getTeamColor() != color) {
-                moves.add(new ChessMove(myPosition, rightEndPosition, null));
-            }
-        }
-    }
+
 
     private void knightLeft(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color, Collection<ChessMove> moves) {
         int x = myPosition.getRow() + 2;
         int y = myPosition.getColumn();
 
         if (x <= 8) {
-            checkY(board, myPosition, color, moves, y, x);
+            checkSides(board, myPosition, color, moves, y, x, "x");
         }
     }
 
@@ -68,22 +51,32 @@ public class KnightMovesCalculator implements PieceMovesCalculator {
         int y = myPosition.getColumn();
 
         if (x >= 1) {
-            checkY(board, myPosition, color, moves, y, x);
+            checkSides(board, myPosition, color, moves, y, x, "x");
         }
     }
 
-    private static void checkY(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color, Collection<ChessMove> moves, int y, int x) {
-        int lowY = y - 1;
-        if (lowY >= 1) {
-            ChessPosition leftEndPosition = new ChessPosition(x, lowY);
+    private static void checkSides(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color, Collection<ChessMove> moves, int side, int forward, String constant) {
+        int leftSide = side - 1;
+        if (leftSide >= 1) {
+            ChessPosition leftEndPosition;
+            if (constant.equals("x")) {
+                leftEndPosition = new ChessPosition(forward, leftSide);
+            } else {
+                leftEndPosition = new ChessPosition(leftSide, forward);
+            }
             ChessPiece leftEnemy = board.getPiece(leftEndPosition);
             if (leftEnemy == null || leftEnemy.getTeamColor() != color) {
                 moves.add(new ChessMove(myPosition, leftEndPosition, null));
             }
         }
-        int highY = y + 1;
-        if (highY <= 8) {
-            ChessPosition rightEndPosition = new ChessPosition(x, highY);
+        int rightSide = side + 1;
+        if (rightSide <= 8) {
+            ChessPosition rightEndPosition;
+            if (constant.equals("x")) {
+                rightEndPosition = new ChessPosition(forward, rightSide);
+            } else {
+                rightEndPosition = new ChessPosition(rightSide, forward);
+            }
             ChessPiece rightEnemy = board.getPiece(rightEndPosition);
             if (rightEnemy == null || rightEnemy.getTeamColor() != color) {
                 moves.add(new ChessMove(myPosition, rightEndPosition, null));
