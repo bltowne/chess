a0f5c5295c4c6e8dc5f0280b3b368675b97ff88e
Bree Towne <bltowne@outlook.com>
2026-07-21 16:49:59 -0700

    imp(p4) write auth access unit tests

--- server/src/test/java/dataaccess/DataAccessTest.java
+++ server/src/test/java/dataaccess/DataAccessTest.java
@@ -1,35 +1,121 @@
 package dataaccess;
 
+import exception.ResponseException;
+import model.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DataAccessTest {
 
-    @Test
-    void createAuthPositive() {}
+    static UserDAO userAccess = new MySqlUserDAO();
+    static GameDAO gameAccess = new MySqlGameDAO();
+    static AuthDAO authAccess = new MySqlAuthDAO();
+
+    @BeforeEach
+    void clear() {
+        userAccess.deleteAllUsers();
+        gameAccess.deleteAllGames();
+        authAccess.deleteAllAuth();
+    }
 
     @Test
-    void createAuthNegative() {}
+    void createAuthPositive() {
+        String expected = "username";
+        AuthData created = authAccess.createAuth(expected);
+        AuthData actual = authAccess.getAuth(created.authToken());
+        Collection<AuthData> auths = authAccess.listAuth();
+
+        assertEquals(expected, actual.username());
+        assertEquals(1, auths.size());
+    }
 
     @Test
-    void getAuthPositive() {}
+    void createAuthNegative() {
+        assertThrows(ResponseException.class, () -> authAccess.createAuth(null));
+    }
 
     @Test
-    void getAuthNegative() {}
+    void getAuthPositive() {
+        String expected = "username";
+        AuthData created = authAccess.createAuth(expected);
+        AuthData actual = authAccess.getAuth(created.authToken());
+
+        assertEquals(expected, actual.username());
+    }
 
     @Test
-    void deleteAuthPositive() {}
+    void getAuthNegative() {
+        assertNull(authAccess.getAuth(""));
+    }
 
     @Test
-    void deleteAuthNegative() {}
+    void deleteAuthPositive() {
+        AuthData created = authAccess.createAuth("username");
+        Collection<AuthData> auths = authAccess.listAuth();
+        assertEquals(1, auths.size());
+
+        authAccess.deleteAuth(created);
+        Collection<AuthData> deletedAuths = authAccess.listAuth();
+        assertEquals(0, deletedAuths.size());
+    }
 
     @Test
-    void listAuthPositive() {}
+    void deleteAuthNegative() {
+        String statement = "DROP TABLE auths";
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.executeUpdate();
+            }
+        } catch (Exception e) {
+            throw new ResponseException(ResponseException.Code.ServerError,
+                    String.format("Error: unable to update database: %s, %s", statement, e.getMessage()));
+        }
+
+        AuthData delete = new AuthData("authToken", "username");
+        assertThrows(ResponseException.class, () -> authAccess.deleteAuth(delete));
+        authAccess = new MySqlAuthDAO();
+    }
 
     @Test
-    void listAuthNegative() {}
+    void listAuthPositive() {
+        authAccess.createAuth("username");
+        Collection<AuthData> auths = authAccess.listAuth();
+
+        assertEquals(1, auths.size());
+    }
 
     @Test
-    void deleteAllAuth() {}
+    void listAuthNegative() {
+        String statement = "DROP TABLE auths";
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.executeUpdate();
+            }
+        } catch (Exception e) {
+            throw new ResponseException(ResponseException.Code.ServerError,
+                    String.format("Error: unable to update database: %s, %s", statement, e.getMessage()));
+        }
+        assertThrows(ResponseException.class, authAccess::listAuth);
+        authAccess = new MySqlAuthDAO();
+    }
+
+    @Test
+    void deleteAllAuth() {
+        authAccess.createAuth("username");
+        authAccess.createAuth("usertwo");
+        Collection<AuthData> auths = authAccess.listAuth();
+        assertEquals(2, auths.size());
+
+        authAccess.deleteAllAuth();
+        Collection<AuthData> deletedAuths = authAccess.listAuth();
+        assertEquals(0, deletedAuths.size());
+    }
 
     @Test
     void getUserPositive() {}
