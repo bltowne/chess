1ba2a1ddec475146155602fe28e3175920d64f78
Bree Towne <bltowne@outlook.com>
2026-07-27 17:50:02 -0700

    imp(p5) set up test file

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,12 +1,15 @@
-package client;
+package java.client;
 
+import exception.ResponseException;
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.ServerFacade;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
@@ -20,10 +23,55 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    void clear() {
+        facade.clear();
+    }
 
     @Test
     public void sampleTest() {
         Assertions.assertTrue(true);
     }
 
+    @Test
+    public void registerPositive() throws ResponseException {}
+
+    @Test
+    public void registerNegative() throws ResponseException {}
+
+    @Test
+    public void loginPositive() throws ResponseException {}
+
+    @Test
+    public void loginNegative() throws ResponseException {}
+
+    @Test
+    public void logoutPositive() throws ResponseException {}
+
+    @Test
+    public void logoutNegative() throws ResponseException {}
+
+    @Test
+    public void createPositive() throws ResponseException {}
+
+    @Test
+    public void createNegative() throws ResponseException {}
+
+    @Test
+    public void joinPositive() throws ResponseException {}
+
+    @Test
+    public void joinNegative() throws ResponseException {}
+
+    @Test
+    public void listPositive() throws ResponseException {}
+
+    @Test
+    public void listNegative() throws ResponseException {}
+
+    @Test
+    public void clearPositive() throws ResponseException {}
+
+    @Test
+    public void clearNegative() throws ResponseException {}
 }
